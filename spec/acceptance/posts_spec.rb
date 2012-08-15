9ca27aa5b4b6ae2c59f982dae702053795b6fcec
karapitan <william@kiranatama.com>
2012-08-15 10:24:47 +0700

    + Post validation spec

--- spec/acceptance/posts_spec.rb
+++ spec/acceptance/posts_spec.rb
@@ -8,4 +8,11 @@ feature 'Posts' do
     page.should have_no_content('New Post')
   end
 
+  scenario 'Post validation' do
+    visit new_post_path
+    click_button 'Create Post'
+    page.should have_content('Title can\'t be blank');
+    page.should have_content('Content can\'t be blank');
+  end
+
 end
